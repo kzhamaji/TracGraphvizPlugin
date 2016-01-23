1512f2209969c1ab01a139cca9c670626fc84c74
Masakazu HAMAJI <kzhamaji@gmail.com>
2016-01-23 19:16:09 +0900

    Fork from r15151 of GraphvizPlugin.

---
+++ graphviz/__init__.py
@@ -0,0 +1,17 @@
+"""
+$Id: __init__.py 1601 2006-11-26 00:00:34Z pkropf $
+$HeadURL: https://trac-hacks.org/svn/graphvizplugin/trunk/graphviz/__init__.py $
+
+Copyright (c) 2005, 2006 Peter Kropf. All rights reserved.
+
+Module file for the graphviz trac wiki processor.
+"""
+
+
+__revision__  = '$LastChangedRevision: 1601 $'
+__id__        = '$Id: __init__.py 1601 2006-11-26 00:00:34Z pkropf $'
+__headurl__   = '$HeadURL: https://trac-hacks.org/svn/graphvizplugin/trunk/graphviz/__init__.py $'
+__docformat__ = 'restructuredtext'
+
+
+from graphviz import *
